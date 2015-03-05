27347e30e9d3fbd3c38e2a5408b849be6f3d2a70
lightning95 <aydar.gizatullin@gmail.com>
2015-03-05 16:20:17 +0300

    Accepted by Niyaz Nigmatullin

--- Java_4sem/Homework_01/src/ru/ifmo/ctddev/gizatullin/walk/RecursiveWalk.java
+++ Java_4sem/Homework_01/src/ru/ifmo/ctddev/gizatullin/walk/RecursiveWalk.java
@@ -7,7 +7,7 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class RecursiveWalk {
     static final int PRIME = 0x01000193;
-    static final int BUFF_SIZE = 2048;
+    static final int BUFF_SIZE = 1024;
     static final int INIT_HASH = 0x811c9dc5;
     static final String ERROR_HASH = String.format("%08x", 0x0);
 
