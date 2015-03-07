fe47af3389588db7f06ac0b638a2621706a1a6b0
lightning95 <aydar.gizatullin@gmail.com>
2015-03-07 18:43:56 +0300

    Initial commit

---
+++ Java_4sem/Homework_03/src/ru/ifmo/ctddev/gizatullin/implementor/Implementor.java
@@ -0,0 +1,13 @@
+package ru.ifmo.ctddev.gizatullin.implementor;
+
+/**
+ * Created by lightning95 on 3/5/15.
+ */
+
+public class Implementor  {
+    public static void main(String[] args) {
+        String s = args[0];
+    }
+
+
+}
