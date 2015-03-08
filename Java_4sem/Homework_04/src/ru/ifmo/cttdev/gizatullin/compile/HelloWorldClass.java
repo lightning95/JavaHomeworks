749382de02386910af4b71de3d89cf3516e4bb74
lightning95 <aydar.gizatullin@gmail.com>
2015-03-08 21:33:36 +0300

    Something works

---
+++ Java_4sem/Homework_04/src/ru/ifmo/cttdev/gizatullin/compile/HelloWorldClass.java
@@ -0,0 +1,10 @@
+package ru.ifmo.cttdev.gizatullin.compile;
+
+/**
+ * Created by lightning95 on 3/8/15.
+ */
+public class HelloWorldClass {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
