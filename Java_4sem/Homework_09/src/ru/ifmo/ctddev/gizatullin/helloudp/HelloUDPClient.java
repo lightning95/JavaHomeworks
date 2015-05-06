9e8335f08f999c2358451a8b94e54b006606379a
lightning95 <aydar.gizatullin@gmail.com>
2015-05-07 00:20:20 +0300

    Tests are passed Docs generated REady for the examining

--- Java_4sem/Homework_09/src/ru/ifmo/ctddev/gizatullin/helloudp/HelloUDPClient.java
+++ Java_4sem/Homework_09/src/ru/ifmo/ctddev/gizatullin/helloudp/HelloUDPClient.java
@@ -11,14 +11,36 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 
 /**
+ * Client that sends request and receives responses, and if response is client is waiting for sends next request.
+ * <p>
+ * Client cab be created from the command line with five parameters:
+ * "url/ip-address port request number_of_threads number_of_requests_per_thread"
+ * <p>
+ * Client begins to send request with the method {@code start}.
+ *
  * @author Aydar Gizatullin a.k.a. lightning95, aydar.gizatullin@gmail.com
  *         Created on 4/29/15.
+ * @see info.kgeorgiy.java.advanced.hello.HelloClient
+ * @see #main
+ * @see #start
  */
 public class HelloUDPClient implements HelloClient {
     private static final String USAGE = "url/ip-address port request number_of_threads number_of_requests_per_thread";
     private static final long TERMINATION_TIME_OUT = 10;
     private static final int SOCKET_TIMEOUT = 50;
 
+    /**
+     * Method to create the client and start it from the command line. Usage for the parameters:
+     * {@value "url/ip-address port request number_of_threads number_of_requests_per_thread"}
+     * url/ip-address - address to send requests to
+     * port - port to send requests to
+     * request - string to send as a request
+     * number_of_threads - number of threads to send requests on
+     * number_of_requests_per_thread - number of the requests to send on one thread
+     *
+     * @param args parameters from the command line
+     * @see #start
+     */
     public static void main(String[] args) {
         if (args == null || args.length == 0 || args.length > 5 ||
                 Arrays.stream(args).anyMatch(Predicate.isEqual(null))) {
@@ -40,8 +62,29 @@ public class HelloUDPClient implements HelloClient {
         }
     }
 
+    /**
+     * Method to start sending requests.
+     *
+     * @param url      address to send requests to
+     * @param port     port to connect to
+     * @param prefix   the prefix of the request
+     * @param requests number of the requests per thread
+     * @param threads  number of the threads to send requests from
+     * @see java.util.concurrent.ExecutorService
+     * @see java.net.InetAddress
+     * @see java.net.DatagramSocket
+     * @see java.net.DatagramPacket
+     */
     @Override
     public void start(String url, int port, String prefix, int requests, int threads) {
+        if (port < 0 || port > 0xFFFF) {
+            System.err.println("Port id is incorrect, has to be between 0 and 65535");
+            return;
+        }
+        if (threads < 1) {
+            System.err.println("Number of threads is non-positive");
+            return;
+        }
         ExecutorService service = Executors.newFixedThreadPool(threads);
         try {
             InetAddress address = InetAddress.getByName(url);
--- Java_4sem/Homework_09/src/ru/ifmo/ctddev/gizatullin/helloudp/HelloUDPServer.java
+++ Java_4sem/Homework_09/src/ru/ifmo/ctddev/gizatullin/helloudp/HelloUDPServer.java
@@ -7,21 +7,48 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.Arrays;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 
 /**
+ * Server that receives request, processes them and sends responses.
+ * Server will reply to every request if the number of threads it is working on allows.
+ * The reply will be {@code "Hello, " + received}, where "receive" is the string representation of the request.
+ * <p>
+ * Server can be created from the command line with two parameters:
+ * port and number of threads.
+ * <p>
+ * Server is started with the method {@code start}.
+ * <p>
+ * To use method start user has to provide two integers - port and threads' number.
+ * <p>
+ * Server can be started several times on different ports.
+ * <p>
+ * To close the server there is the method {@code close}.
+ *
  * @author Aydar Gizatullin a.k.a. lightning95, aydar.gizatullin@gmail.com
  *         Created on 5/6/15.
+ * @see info.kgeorgiy.java.advanced.hello.HelloServer
+ * @see #start
  */
 public class HelloUDPServer implements HelloServer {
-    private static final int BUFFER_SIZE = 256;
+    private static final int BUFFER_SIZE = 1024;
     private static final String USAGE = "Port number_of_threads";
-    private ExecutorService service;
-    private DatagramSocket receivingSocket;
+    private ConcurrentLinkedQueue<DatagramSocket> receivingSockets;
+    private ConcurrentLinkedQueue<ExecutorService> services;
 
+    /**
+     * Method to create class and execute from the command line. Usage for parameters to provide:
+     * {@code port number_of_threads}
+     * port - port to start the server on
+     * number_of_threads - number of threads to process request on
+     *
+     * @param args arguments of the command line
+     * @see #start
+     */
     public static void main(String[] args) {
         if (args == null || args.length < 2 || Arrays.stream(args).anyMatch(Predicate.isEqual(null))) {
             System.out.println(USAGE);
@@ -39,11 +66,36 @@ public class HelloUDPServer implements HelloServer {
         }
     }
 
+    /**
+     * Method to start server with the given parameters
+     *
+     * @param port    number of the port to start server on
+     * @param threads number of threads to process requests on
+     * @see java.util.concurrent.ExecutorService
+     * @see java.net.DatagramSocket
+     * @see java.net.DatagramPacket
+     */
     @Override
     public void start(int port, int threads) {
-        service = Executors.newFixedThreadPool(threads);
+        if (port < 0 || port > 0xFFFF) {
+            System.err.println("Port id is incorrect, has to be between 0 and 65535");
+            return;
+        }
+        if (threads < 1) {
+            System.err.println("Number of threads is non-positive");
+            return;
+        }
+        if (services == null) {
+            services = new ConcurrentLinkedQueue<>();
+        }
+        ExecutorService service = Executors.newFixedThreadPool(threads);
+        services.add(service);
         try {
-            receivingSocket = new DatagramSocket(port);
+            DatagramSocket receivingSocket = new DatagramSocket(port);
+            if (receivingSockets == null) {
+                receivingSockets = new ConcurrentLinkedQueue<>();
+            }
+            receivingSockets.add(receivingSocket);
             for (int i = 0; i < threads; ++i) {
                 service.submit(() -> {
                     try (DatagramSocket sendingSocket = new DatagramSocket()) {
@@ -51,14 +103,9 @@ public class HelloUDPServer implements HelloServer {
                         while (!Thread.interrupted() && !receivingSocket.isClosed()) {
                             receivingSocket.receive(receivingPacket);
                             String received = new String(receivingPacket.getData(), 0, receivingPacket.getLength());
-
-//                            System.out.println("===Received = " + received);
-
                             String sending = "Hello, " + received;
                             sendingSocket.send(new DatagramPacket(sending.getBytes(), sending.getBytes().length,
                                     receivingPacket.getAddress(), receivingPacket.getPort()));
-
-//                            System.out.println("Sent = " + sending);
                         }
                     } catch (IOException ignored) {
                     }
@@ -69,16 +116,26 @@ public class HelloUDPServer implements HelloServer {
         }
     }
 
+    /**
+     * Method to close server
+     *
+     * @see java.util.concurrent.ExecutorService
+     * @see java.net.DatagramSocket
+     */
     @Override
     public void close() {
-        service.shutdown();
-        try {
-            service.awaitTermination(1, TimeUnit.SECONDS);
-            service.shutdownNow();
-        } catch (InterruptedException ignored) {
+        if (services != null) {
+            services.parallelStream().forEach(service -> {
+                service.shutdown();
+                try {
+                    service.awaitTermination(1, TimeUnit.SECONDS);
+                    service.shutdownNow();
+                } catch (InterruptedException ignored) {
+                }
+            });
         }
-        if (receivingSocket != null && !receivingSocket.isClosed()) {
-            receivingSocket.close();
+        if (receivingSockets != null) {
+            receivingSockets.parallelStream().forEach(DatagramSocket::close);
         }
     }
 }
