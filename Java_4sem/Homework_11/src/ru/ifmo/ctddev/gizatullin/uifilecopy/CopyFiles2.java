24a2ebd9722d906fcd9cfac33abd7ef8a868ceb6
lightning95 <aydar.gizatullin@gmail.com>
2015-05-27 01:51:57 +0300

    via ui designer

--- Java_4sem/Homework_11/src/ru/ifmo/ctddev/gizatullin/uifilecopy/CopyFiles2.java
+++ Java_4sem/Homework_11/src/ru/ifmo/ctddev/gizatullin/uifilecopy/CopyFiles2.java
@@ -8,7 +8,7 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 import java.util.EnumSet;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.List;
 
 import static java.nio.file.FileVisitResult.*;
 import static java.nio.file.StandardCopyOption.COPY_ATTRIBUTES;
@@ -19,7 +19,7 @@ import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
  *         Created on 5/13/15.
  */
 
-public class CopyFiles extends SwingWorker<Long, Long> {
+public class CopyFiles2 extends SwingWorker<Long, Long> {
     private static final CopyOption[] FILE_COPY_OPTION_WITH_PROMPT =
             new CopyOption[]{COPY_ATTRIBUTES, REPLACE_EXISTING, ExtendedCopyOption.INTERRUPTIBLE};
     private static final CopyOption[] FILE_COPY_OPTION_WITHOUT_PROMPT =
@@ -31,32 +31,55 @@ public class CopyFiles extends SwingWorker<Long, Long> {
     private static final EnumSet<FileVisitOption> FILE_VISIT_OPTIONS = EnumSet.of(FileVisitOption.FOLLOW_LINKS);
 
     private static final int BUF_SIZE = 65_536;
-
     private final Path source;
     private final Path destination;
-    private final boolean prompt;
+    private final UI.Transfer transfer;
     private final boolean preserve;
+    private final boolean prompt;
+
+    private class Status {
+
+        Status() {
+
+        }
+    }
 
     /**
      * Copy source file to target location. If {@code prompt} is true then
      * prompt user to overwrite target if it exists. The {@code preserve}
      * parameter determines if file attributes should be copied/preserved.
      */
-    void copyFile(Path source, Path target, boolean prompt, boolean preserve, AtomicLong curSize) {
+    void copyFile(Path source, Path target, boolean prompt, boolean preserve) {
         File targetFile = new File(String.valueOf(target));
         if (!prompt || !targetFile.exists()) {
             try (DataInputStream inputStream = new DataInputStream(new FileInputStream(String.valueOf(source)));
                  DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(targetFile))) {
                 Files.setPosixFilePermissions(target, Files.getPosixFilePermissions(source, LinkOption.NOFOLLOW_LINKS));
                 byte[] buf = new byte[BUF_SIZE];
+
+                long lastTime = System.currentTimeMillis();
+                long cur = 0;
                 while (true) {
-                    int res = inputStream.read(buf);
+                    if (!isCancelled()){
+                        return;
+                    }
+                    long res = inputStream.read(buf);
                     if (res == -1) {
                         break;
                     }
-                    curSize.addAndGet(res);
+                    curSize += res;
+                    cur += res;
+                    long curTime = System.currentTimeMillis();
+                    if (curTime - lastTime >= 500) {
+                        publish(cur);
+                        lastTime = curTime;
+                        cur = 0;
+                    }
                     outputStream.write(buf);
-                    setProgress(Math.max(getProgress(), (int) (curSize.get() * 100 / finalSize.get())));
+                    setProgress(Math.max(getProgress(), (int) (curSize * 100 / finalSize)));
+                }
+                if (cur > 0) {
+                    publish(cur);
                 }
 //                Files.copy(source, target, (preserve) ? FILE_COPY_OPTION_WITH_PROMPT : FILE_COPY_OPTION_WITHOUT_PROMPT);
             } catch (IOException x) {
@@ -65,26 +88,67 @@ public class CopyFiles extends SwingWorker<Long, Long> {
         }
     }
 
+    long lastTime;
+    long lastSize;
+
+    public static String timeConvert(long time) {
+        String res;
+        if (time < 60) {
+            res = time + " second" + (time > 1 ? "s" : "");
+        } else if (time < 3600) {
+            res = time / 60 + " minute" + (time / 60 > 1 ? "s" : "");
+        } else {
+            res = time / 3600 + " hour" + (time / 3600 > 1 ? "s" : "");
+        }
+        return res;
+    }
+
+    @Override
+    protected void process(List<Long> chunks) {
+        long curTime = System.currentTimeMillis();
+        long timeLen = (curTime - lastTime);
+        long lCurSize = curSize;
+
+        long el = ((System.currentTimeMillis() - startTime) / 1000);
+        long ave = el > 0 ? lCurSize / (1 << 20) / el : 0;
+        long est = Math.max(ave > 0 ? (finalSize - lCurSize) / (1 << 20) / ave : 0, 0);
+        long cur = lCurSize - lastSize > 0 ? (lCurSize - lastSize) * 1000 / timeLen / (1 << 20) : ave;
+
+        transfer.elapsedTimeLabel.setText("Elapsed time: " + timeConvert(el));
+        transfer.estimatingTimeLabel.setText("Estimating time: " + timeConvert(est));
+        transfer.averageSpeedLabel.setText("Average speed: " + ave + " mb/s");
+        transfer.currentSpeedLabel.setText("Current speed: " + cur + " mb/s");
+        lastSize = lCurSize;
+        lastTime = curTime;
+    }
+
     @Override
     protected void done() {
-        if (!isCancelled()) {
+        /*if (!isCancelled()) {
             long res = (System.currentTimeMillis() - startTime) / 1000;
             JOptionPane.showMessageDialog(null, "Copying is finished \nTime spent: " + UIFileCopy.timeConvert(res));
-        }
+        } else {
+            JOptionPane.showMessageDialog(null, "Copying is canceled");
+        }*/
     }
 
     long startTime;
+    long finalSize;
+    long curSize;
 
     @Override
     protected Long doInBackground() throws Exception {
         startTime = System.currentTimeMillis();
-        final long[] size = {0};
+        final long[] tmp = {0};
         try {
             Files.walkFileTree(source, new SimpleFileVisitor<Path>() {
                 @Override
                 public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                    if (isCancelled()){
+                        return TERMINATE;
+                    }
                     try {
-                        size[0] += attrs.size();
+                        tmp[0] += attrs.size();
                         return super.visitFile(file, attrs);
                     } catch (IOException e) {
                         int res = JOptionPane.showConfirmDialog(null, "Can't read file " +
@@ -102,15 +166,13 @@ public class CopyFiles extends SwingWorker<Long, Long> {
             });
         } catch (IOException ignored) {
         }
-
-        finalSize.set(size[0]);
+        finalSize = tmp[0];
+        curSize = 0;
         try {
-            timer.start();
             Files.walkFileTree(source, FILE_VISIT_OPTIONS, Integer.MAX_VALUE,
-                    new TreeCopier(source, destination, prompt, preserve, curSize));
+                    new TreeCopier(source, destination, prompt, preserve));
         } catch (IOException ignored) {
         }
-        timer.stop();
         return System.currentTimeMillis() - startTime;
     }
 
@@ -122,20 +184,21 @@ public class CopyFiles extends SwingWorker<Long, Long> {
         private final Path target;
         private final boolean prompt;
         private final boolean preserve;
-        private final AtomicLong curSize;
 
-        TreeCopier(Path source, Path target, boolean prompt, boolean preserve, AtomicLong curSize) {
+        TreeCopier(Path source, Path target, boolean prompt, boolean preserve) {
             this.source = source;
             this.target = target;
             this.prompt = prompt;
             this.preserve = preserve;
-            this.curSize = curSize;
         }
 
         @Override
         public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) {
             // before visiting entries in a directory we copy the directory
             // (okay if directory already exists).
+            if (isCancelled()) {
+                return TERMINATE;
+            }
             Path newDir = target.resolve(source.relativize(dir));
             try {
                 Files.copy(dir, newDir, (preserve) ? DIR_COPY_OPTION_WITH_PROMPT : DIR_COPY_OPTION_WITHOUT_PROMPT);
@@ -150,13 +213,19 @@ public class CopyFiles extends SwingWorker<Long, Long> {
 
         @Override
         public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) {
-            copyFile(file, target.resolve(source.relativize(file)), prompt, preserve, curSize);
+            if (isCancelled()) {
+                return TERMINATE;
+            }
+            copyFile(file, target.resolve(source.relativize(file)), prompt, preserve);
             return CONTINUE;
         }
 
         @Override
         public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
             // fix up modification time of directory when done
+            if (isCancelled()) {
+                return TERMINATE;
+            }
             if (exc == null && preserve) {
                 Path newdir = target.resolve(source.relativize(dir));
                 try {
@@ -171,6 +240,9 @@ public class CopyFiles extends SwingWorker<Long, Long> {
 
         @Override
         public FileVisitResult visitFileFailed(Path file, IOException exc) {
+            if (isCancelled()) {
+                return TERMINATE;
+            }
             if (exc instanceof FileSystemLoopException) {
                 System.err.println("cycle detected: " + file);
             } else {
@@ -180,18 +252,11 @@ public class CopyFiles extends SwingWorker<Long, Long> {
         }
     }
 
-    private final AtomicLong finalSize;
-    private final AtomicLong curSize;
-    private final Timer timer;
-
-    public CopyFiles(Path source, Path destination, boolean prompt, boolean preserve,
-                     AtomicLong finalSize, AtomicLong curSize, Timer timer) {
+    public CopyFiles2(Path source, Path destination, boolean prompt, boolean preserve, UI.Transfer transfer) {
         this.source = source;
         this.destination = destination;
         this.prompt = prompt;
         this.preserve = preserve;
-        this.timer = timer;
-        this.finalSize = finalSize;
-        this.curSize = curSize;
+        this.transfer = transfer;
     }
 }
--- Java_4sem/Homework_11/src/ru/ifmo/ctddev/gizatullin/uifilecopy/UIFileCopy.java
+++ Java_4sem/Homework_11/src/ru/ifmo/ctddev/gizatullin/uifilecopy/UIFileCopy.java
@@ -3,14 +3,17 @@ package ru.ifmo.ctddev.gizatullin.uifilecopy;
 import swing.common.BasicAction;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.function.Predicate;
 
 /**
  * @author Aydar Gizatullin a.k.a. lightning95, aydar.gizatullin@gmail.com
@@ -19,19 +22,39 @@ import java.util.concurrent.atomic.AtomicLong;
 public class UIFileCopy {
     public static final String USAGE = "Usage: source destination ";
     private CopyFiles copyFiles;
+    private static final Dimension FRAME_START_DIMENSION = new Dimension(400, 300);
+    private static final Dimension PROGRESSBAR_START_DIMENSION = new Dimension(300, 30);
+    private static final int TIMER_DELAY = 1000;
+
+    public static String timeConvert(long time) {
+        String res;
+        if (time < 60) {
+            res = time + " second" + (time > 1 ? "s" : "");
+        } else if (time < 3600) {
+            res = time / 60 + " minute" + (time / 60 > 1 ? "s" : "");
+        } else {
+            res = time / 3600 + " hour" + (time / 3600 > 1 ? "s" : "");
+        }
+        return res;
+    }
 
     private UIFileCopy(Path source, Path destination) {
         JFrame frame = new JFrame("UIFileCopy");
         frame.setJMenuBar(createMainMenu(frame));
-        frame.setPreferredSize(new Dimension(600, 300));
+//
+        frame.setPreferredSize(FRAME_START_DIMENSION);
+        frame.setMinimumSize(FRAME_START_DIMENSION);
+//
         frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-
+//
         JPanel panel = new JPanel();
         frame.getContentPane().add(panel);
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        panel.setLayout(new GridLayout(6, 1, 10, 10));
+        panel.setBorder(new EmptyBorder(20, 20, 20, 20));
 
         JLabel elapsedTimeLabel = new JLabel();
-        elapsedTimeLabel.setText("Elapsed time: 0 minute(s)");
+        elapsedTimeLabel.setText("Elapsed time: 0 minutes");
 
         JLabel estimatingTimeLabel = new JLabel();
         estimatingTimeLabel.setText("Estimating time: computing...");
@@ -43,7 +66,7 @@ public class UIFileCopy {
         currentSpeedLabel.setText("Current speed: 0 mb/s");
 
         Action cancelAction = new BasicAction("Cancel",
-                "Perform BasicAction", "/toolbarButtonGraphics/general/Help24.gif",
+                "Cancel the operation", "/toolbarButtonGraphics/general/Help24.gif",
                 KeyEvent.VK_C, KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0)) {
             public void actionPerformed(ActionEvent e) {
                 copyFiles.cancel(true);
@@ -52,17 +75,25 @@ public class UIFileCopy {
         };
         JButton cancelButton = new JButton(cancelAction);
         cancelButton.setPreferredSize(new Dimension(50, 30));
+        cancelButton.setMaximumSize(new Dimension(50, 30));
 
         JProgressBar progressBar = new JProgressBar(SwingConstants.HORIZONTAL);
-        progressBar.setPreferredSize(new Dimension(600, 30));
+        progressBar.setStringPainted(true);
+        progressBar.setPreferredSize(PROGRESSBAR_START_DIMENSION);
+        progressBar.setMinimumSize(PROGRESSBAR_START_DIMENSION);
         progressBar.setValue(0);
-
+        
         panel.add(elapsedTimeLabel);
         panel.add(estimatingTimeLabel);
         panel.add(averageSpeedLabel);
         panel.add(currentSpeedLabel);
         panel.add(progressBar);
-        panel.add(cancelButton);
+
+        JPanel buttonPanel = new JPanel();
+//        buttonPanel.setLayout(new FlowLayout());
+        cancelButton.setMaximumSize(new Dimension(70, 30));
+
+        panel.add(buttonPanel.add(cancelButton));
 
         frame.pack();
         frame.setVisible(true);
@@ -72,31 +103,29 @@ public class UIFileCopy {
         AtomicLong curSize = new AtomicLong(0);
 
         long startTime = System.currentTimeMillis();
-        Timer timer = new Timer(1000, e -> {
-            long cur = (System.currentTimeMillis() - startTime) / 1000;
-            elapsedTimeLabel.setText("Elapsed time: " + (cur > 59 ? cur / 60 + " minute(s)" : cur + " second(s)"));
+        Timer timer = new Timer(TIMER_DELAY, e -> {
+            long cur = (System.currentTimeMillis() - startTime) / TIMER_DELAY;
+            elapsedTimeLabel.setText("Elapsed time: " + timeConvert(cur));
         });
 
         final long[] lasts = {0};
 
-        Timer timer2 = new Timer(1000, e -> {
+        Timer timer2 = new Timer(TIMER_DELAY, e -> {
             long lLastSize = lasts[0];
             long lCurSize = curSize.get() / (1 << 20);
 
-            long ave = lCurSize / ((System.currentTimeMillis() - startTime) / 1000);
-            long est = ave > 0 ? (finalSize.get() - lCurSize) / ave : 0;
+            long ave = lCurSize / ((System.currentTimeMillis() - startTime) / TIMER_DELAY);
+            long est = Math.max(ave > 0 ? (finalSize.get() / (1 << 20) - lCurSize) / ave : 0, 0);
             long cur = lCurSize - lLastSize == 0 ? ave : lCurSize - lLastSize;
 
-            estimatingTimeLabel.setText("Estimating time: " + (est > 59 ? est / 60 + " minute(s)" : est + " second(s)"));
+            estimatingTimeLabel.setText("Estimating time: " + timeConvert(est));
             averageSpeedLabel.setText("Average speed: " + ave + " mb/s");
             currentSpeedLabel.setText("Current speed: " + cur + " mb/s");
-            progressBar.setValue((int) (lCurSize * 100 / finalSize.get()));
 
             lasts[0] = lCurSize;
         });
 
-        copyFiles = new CopyFiles(source, destination, false, true,
-                finalSize, curSize, timer2);
+        copyFiles = new CopyFiles(source, destination, false, true, finalSize, curSize, timer2);
 
         copyFiles.addPropertyChangeListener(evt -> {
             if ("progress".equals(evt.getPropertyName())) {
@@ -104,17 +133,17 @@ public class UIFileCopy {
             }
         });
 
-        copyFiles.execute();
         timer.start();
+        copyFiles.execute();
+
         try {
             copyFiles.get();
-            timer.stop();
         } catch (InterruptedException | ExecutionException | CancellationException ignored) {
         }
 
         timer.stop();
         timer2.stop();
-        System.out.println("here");
+
         frame.setVisible(false);
         frame.dispose();
     }
@@ -130,7 +159,7 @@ public class UIFileCopy {
             System.out.println(USAGE);
             return;
         }*/
-        args = new String[]{"/home/lightning95/Studings.Codes/", "/home/lightning95/javatmp"};
+        args = new String[]{"/home/lightning95/Downloads/", "/home/lightning95/javatmp"};
         new UIFileCopy(Paths.get(args[0]), Paths.get(args[1]));
     }
 }
