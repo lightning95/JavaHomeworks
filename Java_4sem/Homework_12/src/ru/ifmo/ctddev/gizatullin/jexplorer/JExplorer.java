5e9e99e7ed7a277335ccc14ff50656f6e64daae0
lightning95 <aydar.gizatullin@gmail.com>
2015-05-28 13:34:45 +0300

    initial commit

---
+++ Java_4sem/Homework_12/src/ru/ifmo/ctddev/gizatullin/jexplorer/JExplorer.java
@@ -0,0 +1,8 @@
+package ru.ifmo.ctddev.gizatullin.jexplorer;
+
+/**
+ * @author Aydar Gizatullin a.k.a. lightning95, aydar.gizatullin@gmail.com
+ *         Created on 5/28/15.
+ */
+public class JExplorer {
+}
